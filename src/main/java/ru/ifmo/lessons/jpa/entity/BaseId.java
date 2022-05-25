693c85f8ab30688c7592679250cdf1a445aa7c55
Anastasiya2808 <anas2210@mail.ru>
2022-05-25 22:32:16 +0300

    +

---
+++ src/main/java/ru/ifmo/lessons/jpa/entity/BaseId.java
@@ -0,0 +1,20 @@
+package ru.ifmo.lessons.jpa.entity;
+
+/* javax.validation -> hibernate-валидатор */
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.ToString;
+
+@ToString
+// родительский класс entity класса,
+// который сам не является entity классом, дб отмечен
+@MappedSuperclass
+
+public abstract class BaseId {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // SERIAL
+    private int id;
+
+}
